244e238d7936ef97564552f02d15a897da7b2fde
eric138148 <eric138148@163.com>
2021-10-24 13:36:38 +0800

    赵云

--- src/test/java/com/example/sb2samples/service/RedisCacheServiceTest.java
+++ src/test/java/com/example/sb2samples/service/RedisCacheServiceTest.java
@@ -63,6 +63,7 @@ public class RedisCacheServiceTest {
         redisCacheService.redisArrayCacheExpireSeconds("key",lists,50);
     }
 
+    //曹操
     @Test
     public void getredisList(){
         List<User> key = redisCacheService.parseRedisArrayCache("key", User.class);
