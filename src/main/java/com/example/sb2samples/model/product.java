56ebf1e984560ca39e077a18701e3cdec5c1fc89
eric138148 <eric138148@163.com>
2021-10-26 13:54:04 +0800

    赵云

---
+++ src/main/java/com/example/sb2samples/model/product.java
@@ -0,0 +1,32 @@
+package com.example.sb2samples.model;
+
+public class product {
+    private int id;
+    private String productName;
+    private int stock;
+
+    public int getId(){
+        return id;
+    }
+    public void setId(int id){
+        this.id=id;
+    }
+    public String getProductName(){
+        return productName;
+    }
+    public void setProductName(String productName){
+        this.productName=productName;
+    }
+
+
+    public int getStock(){
+        return stock;
+    }
+    public void setStock(int stock){
+        this.stock=stock;
+    }
+
+
+
+
+}
